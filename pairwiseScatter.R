bdd2b21e03d3eeae6c309105e8193d61b13a02af
ptdolan <ptdolan@stanford.edu>
2017-02-23 16:10:31 -0800

    Update

--- pairwiseScatter.R
+++ pairwiseScatter.R
@@ -1,15 +1,18 @@
+#R_useful
+DF<-data.frame(iris[,1:4])
+X<-DF
 #pairwiseScatter
 
-DF<-data.frame(randu)
-N<-paste("name",1:length(DF),sep="-")
-colnames(DF)<-N
 
-##
-##pairPlot
-##
-X<-DF
-pairPlot<-function(X=DF,mask=NULL, ggstuff=NULL,cor=TRUE){
-  X[mask]<-NA
+# #
+# pairPlot: pairwise correlation plots for ggplotters.
+# usage: pairPlot(dataframe with columns)
+# #
+
+pairPlot<-function(X,mask=NA, ggstuff=NULL,cor=TRUE){
+  if(!is.na(mask)){
+    X[mask]<-NA
+  }
   stacks<-data.frame()
   names<-colnames(X)
   for (i in 1:(ncol(X)-1)){
@@ -21,12 +24,8 @@ pairPlot<-function(X=DF,mask=NULL, ggstuff=NULL,cor=TRUE){
   }
   stacks$Xvar<-factor(stacks$Xvar)
   stacks$Yvar<-factor(stacks$Yvar)
-  G<-ggplot(stacks)+geom_point(size=0.4,aes(Xval,Yval,color=corr))+facet_grid(Xvar~Yvar)+scale_color_gradient(high = "red",low="darkgrey")
+  G<-ggplot(stacks)+geom_point(size=0.4,aes(Xval,Yval,color=corr))+facet_grid(Xvar~Yvar)+scale_color_gradient2(high = "red",low = 'blue',mid="darkgrey")
   
   plot(G+ggstuff)
-  
   return(stacks)
 }
-
-mask <- DF<0.05
-pairPlot(DF,mask = mask)
